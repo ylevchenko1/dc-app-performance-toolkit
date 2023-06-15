39227012bf1d72c8b11f61d24cf3f2bb04bc4340
ylevchenko <ylevchenko@atlassian.com>
2023-06-15 14:33:38 +0300

    update application_postfix

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -8,35 +8,8 @@ from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def app_specific_action(webdriver):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
-
-    @print_timing("selenium_app_specific_user_login")
-    def measure():
-        def app_specific_user_login(username='admin', password='admin'):
-            login_page = Login(webdriver)
-            login_page.delete_all_cookies()
-            login_page.go_to()
-            login_page.set_credentials(username=username, password=password)
-            if login_page.is_first_login():
-                login_page.first_login_setup()
-            if login_page.is_first_login_second_page():
-                login_page.first_login_second_page_setup()
-            login_page.wait_for_page_loaded()
-        app_specific_user_login(username='admin', password='admin')
-    measure()
-
-    @print_timing("selenium_app_custom_action")
-    def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
