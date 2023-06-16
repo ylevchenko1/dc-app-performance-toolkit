9debae200083f1d9e8ee2aed7eaf2249e51c94a6
ylevchenko <ylevchenko@atlassian.com>
2023-06-16 23:08:29 +0300

    update application_postfix

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -49,13 +49,3 @@ def app_specific_action(webdriver, datasets):
             page.wait_until_visible((By.XPATH, "//div[@data-testid='hero-section-image']"))
         sub_measure()
     measure()
-
-    @print_timing("selenium_app_custom_action")
-    def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
