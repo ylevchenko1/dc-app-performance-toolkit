29222b1203ac125ce350d82074bb0391845cef45
ylevchenko <ylevchenko@atlassian.com>
2023-06-16 22:51:10 +0300

    update application_postfix

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -36,7 +36,7 @@ def app_specific_action(webdriver, datasets):
     def measure():
         @print_timing("selenium_app_custom_action:open_course")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/ac/atlassian-jira-training/app/app")
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/ac/stg-jira-training/app/app")
             page.wait_until_visible((By.ID, "app-root-atlassian-jira-training"))
         sub_measure()
     measure()
@@ -45,7 +45,7 @@ def app_specific_action(webdriver, datasets):
     def measure():
         @print_timing("selenium_app_custom_action:open_course_with_filters")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/ac/atlassian-jira-training/app/app")
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/ac/stg-jira-training/app/app")
             page.wait_until_visible((By.ID, "//div[@data-testid='hero-section-image']"))
         sub_measure()
     measure()
